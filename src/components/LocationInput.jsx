cf5ddc27952a01d69a21b7042617f82be87813bb
Allen M. Jimenez G <allen@optimalbitsoftware.com>
2020-09-07 12:21:10 -0400

    Creating project structure, adding required packages

---
+++ src/components/LocationInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const LocationInput = ({ locations, currentLocation }) => {
+
+  return (
+    <div>
+      Location Input
+    </div>
+  )
+}
+
+export default connect()(LocationInput)
