30eea6f479daf1a29f9b5b3663ffdebf0554d239
Daniel I. Cruz <danielcruz.holguin@gmail.com>
2020-09-09 08:11:15 -0400

    added unity test

--- src/components/FlightBookingDatePicker.jsx
+++ src/components/FlightBookingDatePicker.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import DatePicker from 'react-datepicker'
 import styled from 'styled-components'
-import "react-datepicker/dist/react-datepicker.css";
+import "react-datepicker/dist/react-datepicker";
 import moment from 'moment'
 import { connect } from 'react-redux'
 
